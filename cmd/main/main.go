dc3c615d52d635f48b0d53c2fcaa86a99ea9cf46
Nazarii Savorona <nazar.savorona@gmail.com>
2023-05-28 18:41:56 +0300

    Add getting port from environment variables

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/Digital-Voting-Team/node-connector/pkg/httpserver"
 	"log"
+	"os"
 )
 
 func main() {
 	server := httpserver.NewServer()
+	port := os.Getenv("PORT")
 
-	err := server.Echo.Start(":8080")
+	err := server.Echo.Start(":" + port)
 	if err != nil {
 		log.Panicln(err)
 	}
